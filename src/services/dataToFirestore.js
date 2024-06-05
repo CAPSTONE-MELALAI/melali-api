14a45cf2b04a8af7acf40c22031ba20a2b41f8aa
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2024-06-05 23:47:08 +0800

    feat: add function to upload data json

---
+++ src/services/dataToFirestore.js
@@ -0,0 +1,20 @@
+const { firestore } = require("../services/storeData");
+
+async function uploadData(jsonData) {
+  try {
+    for (const placeData of jsonData) {
+      const docRef = await firestore.collection('destinations').doc();
+
+      const uid = docRef.id;
+      placeData.uid = uid;
+
+      await docRef.set(placeData);
+    }
+    console.log('Data successfully uploaded to Firestore');
+  } catch (error) {
+    console.error('Error uploading data:', error);
+  }
+}
+
+const jsonData = require('../data/datass.json');
+uploadData(jsonData);
