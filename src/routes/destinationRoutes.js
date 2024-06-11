b1a65f5f632269e81d26326c410df2c4d1176198
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2024-06-11 23:02:26 +0800

    feat: add swagger annotation for destination

--- src/routes/destinationRoutes.js
+++ src/routes/destinationRoutes.js
@@ -5,45 +5,58 @@
  *   description: API endpoints for managing destinations
  * components:
  *   schemas:
+ *     Tourism:
+ *       type: object
+ *       properties:
+ *         VisitorFee:
+ *           type: string
+ *           description: Visitor fee information
+ *           example: "$10 per person"
  *     Destination:
  *       type: object
  *       properties:
  *         Tourism:
- *           type: object
- *           properties:
- *             VisitorFee:
- *               type: string
- *               description: Visitor fee information.
+ *           $ref: '#/components/schemas/Tourism'
  *         uid:
  *           type: string
- *           description: The unique identifier of the destination.
+ *           description: The unique identifier of the destination
+ *           example: "dest123"
  *         Description:
  *           type: string
- *           description: A description of the destination.
+ *           description: A description of the destination
+ *           example: "A beautiful beach with crystal clear water."
  *         Category:
  *           type: string
- *           description: The category of the destination.
+ *           description: The category of the destination
+ *           example: "Beach"
  *         Coordinate:
  *           type: string
- *           description: The coordinates of the destination.
+ *           description: The coordinates of the destination
+ *           example: "34.0522 N, 118.2437 W"
  *         Price:
  *           type: number
- *           description: The price in local currency.
+ *           description: The price in local currency
+ *           example: 20
  *         GoogleMapsRating:
  *           type: number
- *           description: The Google Maps rating.
+ *           description: The Google Maps rating
+ *           example: 4.5
  *         GoogleReviewsCount:
  *           type: number
- *           description: The number of Google reviews.
+ *           description: The number of Google reviews
+ *           example: 1234
  *         Place:
  *           type: string
- *           description: The name of the place.
+ *           description: The name of the place
+ *           example: "Santa Monica Beach"
  *         Source:
  *           type: string
- *           description: The source URL.
+ *           description: The source URL
+ *           example: "https://example.com/destinations/santa-monica-beach"
  *         Location:
  *           type: string
- *           description: The location address of the destination.
+ *           description: The location address of the destination
+ *           example: "123 Beach Dr, Santa Monica, CA"
  *       required:
  *         - uid
  *         - Description
@@ -71,9 +84,17 @@
  *             schema:
  *               type: array
  *               items:
- *                 type: object
+ *                 $ref: '#/components/schemas/Destination'
  *       500:
  *         description: Server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 msg:
+ *                   type: string
+ *                   example: "Error getting destinations: some error message"
  */
 
 /**
@@ -89,17 +110,34 @@
  *         description: ID of the destination to retrieve
  *         schema:
  *           type: string
+ *           example: "dest123"
  *     responses:
  *       200:
  *         description: Details of the destination
  *         content:
  *           application/json:
  *             schema:
- *               type: object
+ *               $ref: '#/components/schemas/Destination'
  *       404:
  *         description: Destination not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 msg:
+ *                   type: string
+ *                   example: "Destination not found"
  *       500:
  *         description: Server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 msg:
+ *                   type: string
+ *                   example: "Error getting destination: some error message"
  */
 
 const express = require("express");
